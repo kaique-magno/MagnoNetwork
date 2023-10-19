dc57883b5e2db6ce5f1ba5eaf26166a489c4acd4
Kaique Magno <kaique.magno@hotmail.com>
2023-10-19 18:07:46 -0300

    Add the socketStream as a property to hold in memory while the Performer exists

--- MagnoNetwork/Sources/RequestPerformer.swift
+++ MagnoNetwork/Sources/RequestPerformer.swift
@@ -5,7 +5,8 @@ public protocol RequestPerformer {
     func performSocket<T: Decodable>(task: URLSessionWebSocketTask, withResultType: T.Type) async throws -> AsyncThrowingMapSequence<SocketStream, T>
 }
 
-public struct URLSessionRequestPerformer {
+public class URLSessionRequestPerformer: NSObject {
+    private var socketStream: SocketStream?
     let session: URLSession
     
     public init(session: URLSession = .shared) {
@@ -20,6 +21,7 @@ extension URLSessionRequestPerformer: RequestPerformer {
     
     public func performSocket<T: Decodable>(task: URLSessionWebSocketTask, withResultType: T.Type) async throws -> AsyncThrowingMapSequence<SocketStream, T> {
         let socketStream = SocketStream(task: task)
+        self.socketStream = socketStream
        
         let socketMap: AsyncThrowingMapSequence<SocketStream, T> = socketStream.map { message in
             let object: T = try SocketMessageConverter.object(from: message, objectType: T.self)
